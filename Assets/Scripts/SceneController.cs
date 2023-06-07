f09b9d824f30508414ad1275fed27b98651695f8
Vilmantas Jakniūnas <42962270+Vilmokas@users.noreply.github.com>
2023-06-07 16:16:23 +0300

    feat: back to menu button on game end

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,4 +9,9 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void OnMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
 }
