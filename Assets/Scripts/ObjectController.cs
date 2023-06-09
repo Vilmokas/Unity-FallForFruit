30ae749b4191517564b1c158961a61661a8ed863
Vilmantas Jakniūnas <42962270+Vilmokas@users.noreply.github.com>
2023-06-09 13:19:57 +0300

    feat: add object hit ground particle
    resolves #27

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour
 {
+    [SerializeField] GameObject _destroyParticle;
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Ground"))
@@ -11,6 +13,7 @@ public class ObjectController : MonoBehaviour
             Destroy(gameObject);
             GameObject.Find("Player").GetComponent<Health>().ReduceHealth(1);
             SoundManager.Instance.PlayObjectHitGroundSound();
+            Instantiate(_destroyParticle, transform.position, _destroyParticle.transform.rotation);
         }
     }
 }
